99446dde47defb31aead3348e019ccec226c158e
Default <ahmed.ali.radwan94@gmail.com>
2021-03-31 00:22:12 +0000

    edit

--- src/pick_place_moveit2.cpp
+++ src/pick_place_moveit2.cpp
@@ -1,4 +1,36 @@
-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, PickNik LLC
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik LLC nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 /* Author: Henning Kayser
    Desc: A simple demo node running MoveItCpp for planning and execution
@@ -48,23 +80,10 @@ public:
     geometry_msgs::msg::Pose box_pose;
     box_pose.position.x = 0.4;
     box_pose.position.y = 0.0;
-    box_pose.position.z = 0.8;
+    box_pose.position.z = 1.0;
 
     collision_object.primitives.push_back(box);
     collision_object.primitive_poses.push_back(box_pose);
-    collision_object.operation = collision_object.ADD;
-    
-    shape_msgs::msg::SolidPrimitive box2;
-    box2.type = box.BOX;
-    box2.dimensions = { 0.1, 0.1, 0.1 };
-
-    geometry_msgs::msg::Pose box2_pose;
-    box2_pose.position.x = 0.2;
-    box2_pose.position.y = 0.0;
-    box2_pose.position.z = 1.0;
-
-    collision_object.primitives.push_back(box2);
-    collision_object.primitive_poses.push_back(box2_pose);
     collision_object.operation = collision_object.ADD;
 
     // Add object to planning scene
@@ -75,17 +94,7 @@ public:
 
     // Set joint state goal
     RCLCPP_INFO(LOGGER, "Set goal");
-    
-    geometry_msgs::msg::PoseStamped target_pose1;
-    target_pose1.header.frame_id = "panda_link0";
-    target_pose1.pose.orientation.w = 1.0;
-    target_pose1.pose.position.x = 0.28;
-    target_pose1.pose.position.y = 0.2;
-    target_pose1.pose.position.z = 0.5;
-
-    arm.setGoal(target_pose1, "panda_link8");
-    
-    // arm.setGoal(target_pose1, std::string("end_effector"));  // "extended"
+    arm.setGoal("extended");
 
     // Run actual plan
     RCLCPP_INFO(LOGGER, "Plan to goal");
